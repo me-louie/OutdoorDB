a8a656f76a17d64560c54699f21b79082046a8d2
Lauren <kerk0100@mylaurier.ca>
2021-11-22 09:00:25 -0800

    fixing delete query, adding alert

--- park-user-delete.php
+++ park-user-delete.php
@@ -19,37 +19,19 @@
 
     <?php
 
-        function buildCmdString(&$map) {
-            $arr = [];
-            foreach($_POST as $key => $keyval) {
-                if (array_key_exists($key, $map) && strlen($keyval) > 0) {
-                    array_push($arr, " " . $map[$key] . " = " . $keyval);
-                    }  
-                }
-
-            return implode(",", $arr);
-        }
-
         /**
          * Execute the required DELETE ON CASCADE
          */
         function handleDeleteUserRequest()
         {
             global $db_conn;
-            global $person_map;
-            global $hiker_map;
-            global $camper_map;
-            
-            $cmd_list = array("person" => buildCmdString($person_map), 
-                              "hiker" => buildCmdString($hiker_map), 
-                              "camper" => buildCmdString($camper_map));
-        
-            foreach($cmd_list as $table => $cmdstr) {
-                if (strlen($cmdstr) > 0) {
-                    $format = "DELETE FROM %s WHERE sinnum = %s";
-                    $query = sprintf($format, $table, $cmdstr, $_POST['delete-sin']);
-                    executePlainSQL($query);
-                }
+
+            $cmdstr = "{$_POST['delete-sin']}";
+            if (strlen($cmdstr) > 0) {
+                executePlainSQL("DELETE FROM PERSON WHERE sinnum = $cmdstr");
+            } else {
+                require __DIR__ . '/park-user-division.php';
+                echo alert("SIN cannot be empty!");
             }
 
             OCICommit($db_conn);
